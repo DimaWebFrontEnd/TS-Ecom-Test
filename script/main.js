8f2665808b9f8b98d566d859e9869efc0f8beb16
DimaWebFrontEnd <dmbyndin88@gmail.com>
2025-03-27 14:59:21 +0200

    Test

---
+++ script/main.js
@@ -0,0 +1,26 @@
+const icons = document.querySelectorAll(".icon img");
+const divs = document.querySelectorAll(".questions__info--answers");
+const answers = document.querySelectorAll(".answer")
+console.log(answers)
+
+
+
+const handleAnswers = (icon, index) => {
+   icon.addEventListener("click", e => {
+      divs[index].classList.toggle("active");
+      answers[index].classList.toggle("active");
+      if (divs[index].className === "questions__info--answers active") {
+         e.target.src = "img/Remove_Minus_Circle.png";
+      } else {
+         e.target.src = "img/plus-icon.png"
+      }
+   })
+}
+
+icons.forEach((icon, index) => handleAnswers(icon, index))
+
+
+
+
+
+
